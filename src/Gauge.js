eccc84db157fb15e97450738bdf717d5f3724919
popov7 <deniolp@gmail.com>
2020-04-18 11:12:04 +0300

    Adding arrow

--- src/Gauge.js
+++ src/Gauge.js
@@ -7,7 +7,7 @@ const getCoordsOnArc = (angle, offset = 10) => [
   Math.sin(angle - Math.PI / 2) * offset,
 ];
 
-const Gauge = ({value = 50, min = 0, max = 100, label, units}) => {
+const Gauge = ({value = 10, min = 0, max = 100, label, units}) => {
   const backgroundArc = arc()
     .innerRadius(0.65)
     .outerRadius(1)
@@ -68,6 +68,13 @@ const Gauge = ({value = 50, min = 0, max = 100, label, units}) => {
           stroke="#2c3e50"
           strokeWidth="0.01"
           fill={colorScale(percent)}
+        />
+        <path
+          d="M0.136364 0.0290102C0.158279 -0.0096701 0.219156 -0.00967009 0.241071 0.0290102C0.297078 0.120023 0.375 0.263367 0.375 0.324801C0.375 0.422639 0.292208 0.5 0.1875 0.5C0.0852272 0.5 -1.8346e-08 0.422639 -9.79274e-09 0.324801C0.00243506 0.263367 0.0803571 0.120023 0.136364 0.0290102ZM0.1875 0.381684C0.221591 0.381684 0.248377 0.356655 0.248377 0.324801C0.248377 0.292947 0.221591 0.267918 0.1875 0.267918C0.153409 0.267918 0.126623 0.292947 0.126623 0.324801C0.126623 0.356655 0.155844 0.381684 0.1875 0.381684Z"
+          transform={`rotate(${
+            angle * (180 / Math.PI)
+          }) translate(-0.2, -0.33)`}
+          fill="#6a6a85"
         />
       </svg>
     </div>
