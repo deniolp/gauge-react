04639e851ab9801888b3a2559033b3f275cae83a
popov7 <deniolp@gmail.com>
2020-04-18 12:25:30 +0300

    Making component more abstract

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ import GaugeComponent from './GaugeComponent';
 function App() {
   return (
     <div className="center">
-      <GaugeComponent></GaugeComponent>
+      <GaugeComponent
+        min={0}
+        max={100}
+        initialValue={42}
+        label={`Speed`}
+        units={`kilometers per hour`}
+      ></GaugeComponent>
+      <GaugeComponent
+        min={0}
+        max={30}
+        initialValue={12}
+        label={`Visibility`}
+        units={`kilometers`}
+      ></GaugeComponent>
+      <GaugeComponent
+        min={0}
+        max={4000}
+        initialValue={42}
+        label={`Pressure`}
+        units={`hectopascals`}
+      ></GaugeComponent>
     </div>
   );
 }
--- src/GaugeComponent.js
+++ src/GaugeComponent.js
@@ -2,14 +2,26 @@ import React from 'react';
 
 import Gauge from './Gauge';
 
-const GaugeComponent = ({minSize = 0, maxSize = 100, initialValue = 70}) => {
+const GaugeComponent = ({
+  minSize = 0,
+  maxSize = 100,
+  initialValue = 70,
+  label,
+  units,
+}) => {
   const [value, setValue] = React.useState(initialValue);
   const handleChange = (event) => {
     setValue(event.target.value);
   };
   return (
     <>
-      <Gauge value={value} min={minSize} max={maxSize}></Gauge>
+      <Gauge
+        value={value}
+        min={minSize}
+        max={maxSize}
+        label={label}
+        units={units}
+      ></Gauge>
       <input
         type="range"
         className="range"
